050cf921871fac4e562e292e72c5559777dbe72c
Karl Ezra Pilario <kspilario@up.edu.ph>
2023-03-15 21:58:55 +0800

    Add files via upload

---
+++ kmeans1.m
@@ -0,0 +1,36 @@
+% Sample data set
+x = [mvnrnd([1 3],0.5*eye(2),100);
+     mvnrnd([-2 1],0.3*eye(2),100);
+     mvnrnd([2 -1],0.7*eye(2),100)];
+   
+%% Initialize K-means
+x = (x - mean(x))./std(x);              % Normalize the data (optional)
+tol = 1e-3;                             % Set a tolerance
+K = 3;                                  % Assumed no. of clusters
+mu = x(randi(length(x),K,1),:);         % Initial guess of centroids
+close all; plotdata(x,mu); pause(1);    % Plot the current status
+ 
+%% K-means Clustering
+mu0 = zeros(size(mu)); iter = 0;        % Save the old centroids here
+ 
+while sum(abs(mu0-mu),'all') > tol
+    iter = iter + 1; mu0 = mu;          % Save the old centroids to mu0
+    
+    % Assign each sample to a centroid
+    [~,ind] = min(pdist2(x,mu,'Euclidean'),[],2);
+    
+    % Compute the new centroids
+    mu = cell2mat(arrayfun(@(j) ...
+        mean(x(ind == j,:)),(1:K)','Uni',false));
+    fprintf('Iteration %d:\n',iter);
+    disp(mu); plotdata(x,mu); pause(0.1);
+end
+ 
+%% For Plotting the Data and Voronoi Diagram
+function plotdata(x,mu)
+    scatter(x(:,1),x(:,2),12,'b','filled');         % Scatter plot of
+    box on; axis([-1 1 -1 1]*3); hold on;           %   normalized data
+    [vx,vy] = voronoi(mu(:,1),mu(:,2));             % Voronoi edges
+    scatter(mu(:,1),mu(:,2),'r','filled');          % Plot the centroids
+    plot(vx,vy,'k-','LineWidth',1.2); hold off;     % Plot the edges
+end
